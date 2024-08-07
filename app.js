9195e6a71299be7857d9b8e669445632c2d36b3a
vikasgaddam9010 <vikasgaddam9010@gmail.com>
2024-08-07 12:51:52 +0000

    camelCased

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ const dbpath = path.join(__dirname, 'covid19IndiaPortal.db')
 const connection = async () => {
   try {
     db = await open({filename: dbpath, driver: sqlite3.Database})
-    app.listen(3000, () => {
-      console.log('Server is running')
-    })
   } catch (e) {
     console.log('connection error : ' + e)
     process.exit(1)
@@ -38,6 +35,7 @@ const check = (req, res, next) => {
   } else {
     jwttoken.verify(jwt, 'secretkey', async (err, payload) => {
       if (err) {
+        res.status(401)
         res.send('Invalid JWT Token')
       } else {
         //console.log('correct jwt token')
@@ -66,8 +64,8 @@ app.post('/login/', async (req, res) => {
         const payload = {
           username: username,
         }
-        const token = jwttoken.sign(payload, 'secretkey')
-        res.send({token})
+        const jwtToken = jwttoken.sign(payload, 'secretkey')
+        res.send({jwtToken})
       }
     }
   } catch (e) {
@@ -75,11 +73,19 @@ app.post('/login/', async (req, res) => {
   }
 })
 
+const convertCamelCase = each => {
+  return {
+    stateId: each.state_id,
+    stateName: each.state_name,
+    population: each.population,
+  }
+}
+
 app.get('/states', check, async (req, res) => {
   try {
     const api2 = `SELECT * FROM state;`
     const ans = await db.all(api2)
-    res.send(ans)
+    res.send(ans.map(each => convertCamelCase(each)))
   } catch (e) {
     console.log('get api error : ' + e)
   }
@@ -89,7 +95,7 @@ app.get('/states/:stateId', check, async (req, res) => {
   const {stateId} = req.params
   const api3 = `SELECT * FROM state WHERE state_id='${stateId}';`
   const ans = await db.get(api3)
-  res.send(ans)
+  res.send(convertCamelCase(ans))
 })
 
 app.post('/districts/', check, async (req, res) => {
@@ -102,11 +108,23 @@ app.post('/districts/', check, async (req, res) => {
   res.send('District Successfully Added')
 })
 
+const convertDistrictsTOCamelCase = each => {
+  return {
+    districtId: each.district_id,
+    districtName: each.district_name,
+    stateId: each.state_id,
+    cases: each.cases,
+    cured: each.cured,
+    active: each.active,
+    deaths: each.deaths,
+  }
+}
+
 app.get('/districts/:districtId/', check, async (req, res) => {
   const {districtId} = req.params
   const api5 = `SELECT * FROM district WHERE district_id = '${districtId}';`
   const ans = await db.get(api5)
-  res.send(ans)
+  res.send(convertDistrictsTOCamelCase(ans))
 })
 
 app.delete('/districts/:districtId/', check, async (req, res) => {
@@ -130,5 +148,7 @@ app.get('/states/:stateId/stats', check, async (req, res) => {
   const ans = await db.get(api8)
   res.send(ans)
 })
+
+app.listen(3000)
 
 module.exports = app
